cd68b326b5f5beef8f8ce69d2d3347f707cf5782
申诺呦 <shennuo123@163.com>
2023-08-24 16:37:42 +0800

    axios基础封装

--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+//测试axios接口函数
+import {getCategory} from '@/apis/testapi'
+getCategory().then(res=>{
+    console.log(res)
+})
+
+
 const app = createApp(App)
 
 app.use(createPinia())
