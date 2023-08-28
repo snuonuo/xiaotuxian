4d5067bdd30c02033edeb2f87beb758981af625e
申诺呦 <shennuo123@163.com>
2023-08-28 15:15:45 +0800

    Layout-吸顶导航的交互实现

--- src/main.js
+++ src/main.js
@@ -9,11 +9,7 @@ import router from './router'
 // 引入初始化的样式文件
 import '@/styles/common.scss'
 
-//测试axios接口函数
-import {getCategory} from '@/apis/testapi'
-getCategory().then(res=>{
-    console.log(res)
-})
+
 
 
 const app = createApp(App)
