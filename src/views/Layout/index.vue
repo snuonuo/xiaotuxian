d479b5836e86989c1c81e7cd92f7150820e3a415
申诺呦 <shennuo123@163.com>
2023-08-28 16:44:13 +0800

    pinia优化重复请求

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -3,6 +3,12 @@ import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
 import LayoutFixed from './components/LayoutFixed.vue';
+
+// 获取导航列表的action
+import {useCategoryStore} from '@/stores/category'
+const categoryStore = useCategoryStore()
+import {onMounted} from 'vue'
+onMounted(() => categoryStore.getCategory())
 </script>
 
 <template>
