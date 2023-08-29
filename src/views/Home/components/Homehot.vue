708efd90a2175934df5e5fbf891ec6280610704e
申诺呦 <shennuo123@163.com>
2023-08-29 20:00:27 +0800

    使用panel实现热门商品

--- src/views/Home/components/Homehot.vue
+++ src/views/Home/components/Homehot.vue
@@ -1,34 +1,39 @@
 <script setup>
+import HomePanel from './HomePanel.vue'
+import { getHotAPI } from '@/apis/home'
+import { ref } from 'vue'
+const hotList = ref([])
+const getHotList = async () => {
+  const res = await getHotAPI()
+  hotList.value = res.result
+}
+getHotList()
 
 </script>
 
 <template>
-  <div></div>
-  <!-- 下面是插槽主体内容模版
-  <ul class="goods-list">
-    <li v-for="item in newList" :key="item.id">
-      <RouterLink to="/">
-        <img :src="item.picture" alt="" />
-        <p class="name">{{ item.name }}</p>
-        <p class="price">&yen;{{ item.price }}</p>
-      </RouterLink>
-    </li>
-  </ul>
-  -->
+  <HomePanel title="人气推荐" sub-title="人气爆款 不容错过">
+      <ul class="goods-list">
+        <li v-for="item in hotList" :key="item.id">
+          <RouterLink to="/">
+            <img v-img-lazy="item.picture" alt="">
+            <p class="name">{{ item.title }}</p>
+            <p class="desc">{{ item.alt }}</p>
+          </RouterLink>
+        </li>
+      </ul>
+  </HomePanel>
 </template>
-
 
 <style scoped lang='scss'>
 .goods-list {
   display: flex;
   justify-content: space-between;
-  height: 406px;
+  height: 426px;
 
   li {
     width: 306px;
     height: 406px;
-
-    background: #f0f9f4;
     transition: all .5s;
 
     &:hover {
@@ -45,13 +50,11 @@
       font-size: 22px;
       padding-top: 12px;
       text-align: center;
-      text-overflow: ellipsis;
-      overflow: hidden;
-      white-space: nowrap;
     }
 
-    .price {
-      color: $priceColor;
+    .desc {
+      color: #999;
+      font-size: 18px;
     }
   }
 }
